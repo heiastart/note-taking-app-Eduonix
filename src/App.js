633ce9c86c2fc300d92c5a376ecfe2c760b24a78
Oyvind Sorlie <heiastart@outlook.com>
2021-11-29 20:58:53 +0100

    Added a typography label/heading

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import MenuAppBar from './components/Header';
+import { Typography } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
       <MenuAppBar/>
+      <br/>
+      <Typography variant="h4" component="h5">Take your Note</Typography>
     </div>
   );
 }
